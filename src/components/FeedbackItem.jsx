4b5ed9f585c1c95ad98f773632382a6403f83a4c
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-11-30 06:55:48 +0530

    modified:   src/App.js 	modified:   src/components/FeedbackItem.jsx 	new file:   src/components/FeedbackList.jsx 	new file:   src/data/FeedbackData.js

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -5,18 +5,10 @@ function FeedbackItem(){
     const [rating,setRating] = useState(7)
     const [text, setText] = useState('This is a example')
 
-    const handleClick = () => {
-        setRating((prev) => {
-            console.log("prev")
-            return prev + 1
-        })
-    }
-
     return (
         <div className="card">
             <div className="num-display">{rating}</div>
             <div className="text-display">{text}</div>
-            <button onClick={handleClick}>Click</button>
         </div>
     )
 }
