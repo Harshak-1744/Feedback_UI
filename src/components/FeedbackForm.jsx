00220d02bcaee904b4f693307e7bce3f86179636
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-03 20:51:20 +0530

    modified:   src/App.js 	modified:   src/components/FeedbackForm.jsx

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,13 @@ import Card from './shared/Card'
 export default function FeedbackForm() {
   return (
     <Card>
-        Form
+        <form>
+          <h2>How do you Rate our Service</h2>
+          <div className='input-group'>
+            <input type='text' placeholder='Write your Review' />
+            <button type='submit'>send</button>
+          </div>
+        </form>
     </Card>
   )
-}
+} 
