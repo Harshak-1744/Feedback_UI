1bd41b598914fb9e8faf62112cf48171c4601203
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-04 21:37:33 +0530

    modified:   src/components/FeedbackForm.jsx 	new file:   src/components/shared/Button.jsx

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,12 +1,19 @@
+import {useState} from 'react'
 import Card from './shared/Card'
 
 export default function FeedbackForm() {
+  const [text, setText] = useState('')
+
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+
   return (
     <Card>
         <form>
           <h2>How do you Rate our Service</h2>
           <div className='input-group'>
-            <input type='text' placeholder='Write your Review' />
+            <input onChange={handleTextChange} type='text' placeholder='Write your Review' value={text}/>
             <button type='submit'>send</button>
           </div>
         </form>
