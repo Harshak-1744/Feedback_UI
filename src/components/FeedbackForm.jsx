febc8b5ec70de733a13799cab1f9844f47c3122e
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-04 22:12:50 +0530

    modified:   src/components/FeedbackForm.jsx

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,22 @@ import Button from './shared/Button'
 
 export default function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
+    
+    if(text === ''){
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be atleast 10 characters')
+      setBtnDisabled(true)
+    }else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+    
     setText(e.target.value)
   }
 
@@ -15,8 +29,10 @@ export default function FeedbackForm() {
           <h2>How do you Rate our Service</h2>
           <div className='input-group'>
             <input onChange={handleTextChange} type='text' placeholder='Write your Review' value={text}/>
-            <Button type='submit'>send</Button>
+            <Button type='submit' isDisabled={btnDisabled}>SEND</Button>
           </div>
+
+          {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
