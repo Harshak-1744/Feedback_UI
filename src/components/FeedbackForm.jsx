c916418ae53dfaf0b6c021b1d743067773deb283
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-05 19:38:40 +0530

    modified:   src/App.js 	new file:   src/components/AboutIconLink.jsx 	modified:   src/components/FeedbackForm.jsx 	modified:   src/components/FeedbackItem.jsx 	modified:   src/components/FeedbackList.jsx 	modified:   src/components/FeedbackStats.jsx 	modified:   src/components/Header.jsx 	modified:   src/components/RatingSelect.jsx 	new file:   src/components/assets/spinner.gif 	modified:   src/components/shared/Button.jsx 	new file:   src/components/shared/Card.jsx 	new file:   src/components/shared/Spinner.jsx 	new file:   src/context/FeedbackContext.js 	modified:   src/data/FeedbackData.js 	modified:   src/index.css 	modified:   src/index.js 	new file:   src/pages/AboutPage.jsx

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,38 +1,88 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackStats from './components/FeedbackStats'
-import FeedbackForm from './components/FeedbackForm'
-import AboutIconLink from './components/AboutIconLink'
-import AboutPage from './pages/AboutPage'
-import { FeedbackProvider } from './context/FeedbackContext'
+import { useState, useContext, useEffect } from 'react'
+import RatingSelect from './RatingSelect'
+import Card from './shared/Card'
+import Button from './shared/Button'
+import FeedbackContext from '../context/FeedbackContext'
 
-function App() {
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext)
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
+    }
+  }, [feedbackEdit])
+
+  // NOTE: This should be checking input value not state as state won't be the updated value until the next render of the component
+
+  // prettier-ignore
+  const handleTextChange = ({ target: { value } }) => { // 👈  get the value
+    if (value === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+      
+  // prettier-ignore
+    } else if (value.trim().length < 10) { // 👈 check for less than 10
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+    setText(value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback)
+      } else {
+        addFeedback(newFeedback)
+      }
+
+      // NOTE: reset to default state after submission
+      setBtnDisabled(true) // 👈  add this line to reset disabled
+      setRating(10) //👈 add this line to set rating back to 10
+      setText('')
+    }
+  }
+
+  // NOTE: pass selected to RatingSelect so we don't need local duplicate state
   return (
-    <FeedbackProvider>
-      <Router>
-        <Header />
-        <div className='container'>
-          <Routes>
-            <Route
-              path='/'
-              element={
-                <>
-                  <FeedbackForm />
-                  <FeedbackStats />
-                  <FeedbackList />
-                </>
-              }
-            ></Route>
-
-            <Route path='/about' element={<AboutPage />} />
-          </Routes>
-
-          <AboutIconLink />
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={setRating} selected={rating} />
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
-      </Router>
-    </FeedbackProvider>
+
+        {message && <div className='message'>{message}</div>}
+      </form>
+    </Card>
   )
 }
 
-export default App
+export default FeedbackForm
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,32 +1,46 @@
-import PropTypes from 'prop-types'
-import FeedbackItem from "./FeedbackItem"
+import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
+import FeedbackItem from './FeedbackItem'
+import Spinner from './shared/Spinner'
+import FeedbackContext from '../context/FeedbackContext'
 
+// NOTE: added layout prop for nicer animation
+// https://www.framer.com/docs/animation/#layout-animations
 
-function FeedbackList({feedback, handleDelete}) {
+function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-    if(!feedback || feedback.length === 0) {
-        return <p>There is no Feedback Yet</p>
-    }
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>
+  }
 
-    return (
-        <div  className="feedback-list">
-            {feedback.map((item)=>(
-                <FeedbackItem key={item.id} item={item} handleDelete= {handleDelete}/>
-             ))}
-        </div>
-    )
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  )
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired
-        })
-    )
-}
-
-
-export default  FeedbackList
+export default FeedbackList
