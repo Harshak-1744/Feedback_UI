2a3c0bf072bb341fb2ecd30dc5b360781a3b706c
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-06 18:25:27 +0530

    modified:   src/components/Header.jsx 	modified:   src/index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,8 @@ const Header = ({ text, bgColor, textColor }) => {
     };
     const headerTextStyles = {
         color: textColor,
-        textDecoration: 'none',
+        textDecoration: 'underline',
+        textUnderlineOffset: '0.4em',
     };
     return (
         <header style={headerStyles}>
